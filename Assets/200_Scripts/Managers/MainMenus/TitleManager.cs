2919b86bc8366aa5ace12bd85b2b9e724a92e380
junhaddi <rkdwnsgk05@gmail.com>
2024-11-06 21:09:49 +0900

    WIP UI 개편중

--- Assets/200_Scripts/Managers/MainMenus/TitleManager.cs
+++ Assets/200_Scripts/Managers/MainMenus/TitleManager.cs
@@ -5,52 +5,102 @@ using UnityEngine.SceneManagement;
 public class TitleManager : MonoBehaviour
 {
     // 컴포넌트
+    [SerializeField] private GameObject renameModal;
     [SerializeField] private SceneTransition newGameSceneTransition;
-    [SerializeField] private TMP_InputField playerNameInputField;
+
+    private TMP_InputField renameModalInputField;
+
+    private bool isModalOpen;
+
+    private void Awake()
+    {
+        renameModalInputField = renameModal.GetComponentInChildren<TMP_InputField>();
+    }
 
     private void Start()
     {
         GameplayManager.Instance.isGameRunning = false;
-        UpdatePlayerName();
-    }
-
-    public void OnPlayerNameChanged(string newValue)
-    {
-        ProfileManager.Instance.playerProfile.playerName = newValue;
-        ProfileManager.Instance.SaveProfile();
     }
 
     public void OnClickNewGameButton()
     {
-        GameplayManager.Instance.isGameRunning = true;
-        TransitionManager.Instance.LoadScene(newGameSceneTransition);
+        if (!isModalOpen)
+        {
+            GameplayManager.Instance.isGameRunning = true;
+            TransitionManager.Instance.LoadScene(newGameSceneTransition);
+        }
     }
 
     public void OnClickContinueButton()
     {
-        if (!string.IsNullOrEmpty(ProfileManager.Instance.playerProfile.progressSave.playerSpawnpoint.sceneTransition.sceneName))
+        if (!isModalOpen)
         {
-            GameplayManager.Instance.isGameRunning = true;
-            TransitionManager.Instance.LoadSceneWithPlayer(ProfileManager.Instance.playerProfile.progressSave.playerSpawnpoint);
+            if (!string.IsNullOrEmpty(ProfileManager.Instance.playerProfile.progressSave.playerSpawnpoint.sceneTransition.sceneName))
+            {
+                GameplayManager.Instance.isGameRunning = true;
+                TransitionManager.Instance.LoadSceneWithPlayer(ProfileManager.Instance.playerProfile.progressSave.playerSpawnpoint);
+            }
         }
     }
 
     public void OnClickLeaderboardButton()
     {
-        SceneManager.LoadScene("Scene_Leaderboard");
+        if (!isModalOpen)
+        {
+            SceneManager.LoadScene("Scene_Leaderboard");
+        }
     }
 
     public void OnClickExitButton()
     {
+        if (!isModalOpen)
+        {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #else
             Application.Quit();
 #endif
+        }
     }
 
-    private void UpdatePlayerName()
+    public void OnClickConfirmRenameModal()
     {
-        playerNameInputField.text = ProfileManager.Instance.playerProfile.playerName;
+        if (isModalOpen)
+        {
+            ProfileManager.Instance.playerProfile.playerName = renameModalInputField.text;
+            ProfileManager.Instance.SaveProfile();
+
+            renameModal.SetActive(false);
+
+            // TODO : 블러 효과 해제
+
+            isModalOpen = false;
+        }
+    }
+
+    public void OnClickCancelRenameModal()
+    {
+        if (isModalOpen)
+        {
+            renameModal.SetActive(false);
+
+            // TODO : 블러 효과 해제
+
+            isModalOpen = false;
+        }
+    }
+
+    private void CreateRenameModal()
+    {
+        if (!isModalOpen)
+        {
+            renameModalInputField.text = ProfileManager.Instance.playerProfile.playerName;
+
+            renameModal.SetActive(true);
+
+            // TODO : 블러 효과 적용
+
+            isModalOpen = true;
+        }
     }
 }
