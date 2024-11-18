2e710eadd88fb7144e5c377d27a99577418576e0
junhaddi <rkdwnsgk05@gmail.com>
2024-11-19 01:06:39 +0900

    "벨트" 기믹 버그 수정

--- Assets/200_Scripts/GameplayObjects/Types/05.Platforms/BeltPlatform.cs
+++ Assets/200_Scripts/GameplayObjects/Types/05.Platforms/BeltPlatform.cs
@@ -3,10 +3,27 @@ using UnityEngine;
 public class BeltPlatform : MonoBehaviour
 {
     public CommonEnums.RotationDirection rotationDirection;
-    public float beltSpeed = 2f;
+    public float beltSpeed = 8f;
+    public float gearRotationMultiplier = 40f;
+
+    private Transform[] gearTransforms;
 
     private void Start()
     {
-        // TODO : rotationDirection 에 따라 애니메이션 역재생 여부 결정
+        // "Gear"로 시작하는 자식들의 Transform을 찾아서 저장
+        gearTransforms = GetComponentsInChildren<Transform>();
+        gearTransforms = System.Array.FindAll(gearTransforms, t => t.name.StartsWith("Gear"));
+    }
+
+    private void Update()
+    {
+        // 회전 방향에 따라 회전
+        float rotationSpeed = beltSpeed * gearRotationMultiplier * Time.deltaTime;
+        float rotationAngle = rotationDirection == CommonEnums.RotationDirection.Clockwise ? -rotationSpeed : rotationSpeed;
+
+        foreach (Transform gear in gearTransforms)
+        {
+            gear.Rotate(0, 0, rotationAngle);
+        }
     }
 }
