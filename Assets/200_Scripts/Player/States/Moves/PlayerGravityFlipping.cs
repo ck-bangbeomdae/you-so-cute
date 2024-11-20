1cf4f6417bdef8a638205dc46ca6b510cd84140c
CatAria8484 <cataria8484@gmail.com>
2024-11-21 01:55:12 +0900

    바닥 먼지 파티클 실행 조건 변경

--- Assets/200_Scripts/Player/States/Moves/PlayerGravityFlipping.cs
+++ Assets/200_Scripts/Player/States/Moves/PlayerGravityFlipping.cs
@@ -11,29 +11,29 @@ public class PlayerGravityFlipping : BaseState<Player>
     public override void Execute(Player player)
     {
         // 중력 반전시 먼지 떨어지는 파티클 재생
-        if (player.r_dustParticlePrefab != null && player.s_dustParticlePrefab != null)
-        {
-            if (player.IsGravityFlipped)
-            {
-                player.s_dustParticlePrefab.GetComponent<ParticleSystem>().Stop();
-                player.r_dustParticlePrefab.GetComponent<ParticleSystem>().Play();
+        //if (player.r_dustParticlePrefab != null && player.s_dustParticlePrefab != null)
+        //{
+        //    if (player.IsGravityFlipped)
+        //    {
+        //        player.s_dustParticlePrefab.GetComponent<ParticleSystem>().Stop();
+        //        player.r_dustParticlePrefab.GetComponent<ParticleSystem>().Play();
 
-                foreach (GameObject p in player.r_flipDustParticlePrefab)
-                {
-                    p.GetComponent<ParticleSystem>().Play();
-                }
-            }
-            else
-            {
-                player.r_dustParticlePrefab.GetComponent<ParticleSystem>().Stop();
-                player.s_dustParticlePrefab.GetComponent<ParticleSystem>().Play();
+        //        foreach (GameObject p in player.r_flipDustParticlePrefab)
+        //        {
+        //            p.GetComponent<ParticleSystem>().Play();
+        //        }
+        //    }
+        //    else
+        //    {
+        //        player.r_dustParticlePrefab.GetComponent<ParticleSystem>().Stop();
+        //        player.s_dustParticlePrefab.GetComponent<ParticleSystem>().Play();
 
-                foreach (GameObject p in player.s_flipDustParticlePrefab)
-                {
-                    p.GetComponent<ParticleSystem>().Play();
-                }
-            }
-        }
+        //        foreach (GameObject p in player.s_flipDustParticlePrefab)
+        //        {
+        //            p.GetComponent<ParticleSystem>().Play();
+        //        }
+        //    }
+        //}
 
         player.playerStateMachine.ChangeState(player.playerStates[PlayerState.InAir]);
     }
